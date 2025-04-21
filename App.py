54c29bd791fcbbd121e9bd9d87e5a54e02aa7c06
JSandfoss08 <jared.sandfoss039@topper.wku.edu>
2025-04-20 23:22:42 -0500

    modified:   App.py

--- App.py
+++ App.py
@@ -3,8 +3,7 @@ from flask import Flask, render_template, redirect, request
 from flask_scss import Scss
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-from sqlalchemy import func, text, Table, Column, CHAR, DECIMAL, DATE, create_engine, insert, ForeignKey, select
-from sqlalchemy.orm import Mapped, MappedColumn, relationship
+from sqlalchemy import func, text, CHAR, DECIMAL, DATE, ForeignKey, select
 
 #Creating a flask instance
 app = Flask(__name__)
